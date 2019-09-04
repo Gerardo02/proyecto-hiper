e987e91ed0529f3da180b93ae67e9bedd504eea8
gerardo jerez <gerardo.jerez@hotmail.com>
2019-09-04 09:21:29 -0700

    Tamaños y colores

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 const buttons = document.querySelectorAll('button');
+let button = document.getElementById('grande');
+let height = 500;
+
+buttons.forEach(
+    button => {
+        button.style.height = `${height}px`;
+        height = height - 20;
+        button.style.backgroundColor = button.dataset.colors;
+    }
+);
