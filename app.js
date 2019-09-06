2bca4756a742d598e960a228feeff962706ba38b
jesus.abarron.z <jesus.abarron.z@gmail.com>
2019-09-06 12:25:58 -0700

    notas que suenan

--- app.js
+++ app.js
@@ -8,3 +8,14 @@ buttons.forEach(
         button.style.backgroundColor = button.dataset.colors;
     }
 );
+
+const playnote = event =>{
+    const button = event.target;
+    const note = button.dataset.note;
+    const audioId = `audio${note}`;
+    console.log(audioId);
+    const audio = document.getElementById(audioId);
+    audio.pause();
+    audio.currentTime = 0;
+    audio.play();
+}
